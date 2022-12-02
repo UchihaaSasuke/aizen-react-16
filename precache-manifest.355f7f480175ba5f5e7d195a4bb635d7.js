c56d368e035003ded83e2f1b29816a1d40943dff
UchihaaSasuke <cdrevision@gmail.com>
2022-12-02 18:04:58 +0100

    Updates

---
+++ precache-manifest.355f7f480175ba5f5e7d195a4bb635d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fcb59fbfe279587fbe01ec7fffe566",
+    "url": "/aizen-react-16/index.html"
+  },
+  {
+    "revision": "f1c4a29c2d7f4b0adc68",
+    "url": "/aizen-react-16/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "d3f3324e38f50413353d",
+    "url": "/aizen-react-16/static/js/2.5e60c65c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aizen-react-16/static/js/2.5e60c65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c4a29c2d7f4b0adc68",
+    "url": "/aizen-react-16/static/js/main.a47f9954.chunk.js"
+  },
+  {
+    "revision": "b7b3aa7398d59478fe21",
+    "url": "/aizen-react-16/static/js/runtime-main.7617945a.js"
+  }
+]);
